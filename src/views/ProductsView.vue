ca02927923588c2abf95d9eb9738fe39da0b98eb
Marianne Anctil <2163768@csfoy.ca>
2023-12-13 10:31:37 -0500

    depart

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,35 @@
+<script setup>
+import ProductsList from '../components/ProductsList.vue'
+</script>
+
+<template>
+  <div>
+<ProductsList :products='products' :productsPage="5" />
+  </div>
+</template>
+
+<script>
+
+  export default {
+  data(){
+    return{
+      products:null
+  }
+  },
+
+  methods: {
+    async fetchData(){
+      const response = await fetch('https://storerestservice.azurewebsites.net/api/products/');
+      this.products = await response.json()
+    }
+  },
+
+  created(){
+    this.fetchData();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
