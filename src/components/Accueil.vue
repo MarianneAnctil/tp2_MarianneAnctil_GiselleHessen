04de2e3cd3ca4f59b7e06ffa24e543bf489e55b3
Marianne Anctil <2163768@csfoy.ca>
2023-12-19 14:13:09 -0500

    afficher 3 films

---
+++ src/components/Accueil.vue
@@ -0,0 +1,52 @@
+
+
+<script>
+export default{
+
+props: {
+    movies: {
+        type: Array,
+    //default: () => [],
+    },
+   randomizedNumber:{
+        type:Number,
+        default:1,
+    },
+    selectedMovies:{
+        type:Array,
+       // default: () => [],
+    }
+},
+computed:{
+    randomizeSelectedMovies(){
+    console.log('Computing randomizeSelectedMovies...');
+        let selectedMovies = [];
+        
+for(let cpt=0; cpt<3; cpt++){
+    console.log(selectedMovies)
+    console.log(this.movies)
+const randomizedNumber = Math.floor(Math.random() * this.movies.length);
+selectedMovies.push(this.movies[randomizedNumber])
+console.log(selectedMovies)
+console.log(randomizedNumber)
+console.log(this.movies)
+}
+return selectedMovies;
+    }
+},
+};
+
+
+
+
+</script>
+
+
+<template>
+<ul>
+    <li
+    v-for="movie in randomizeSelectedMovies" :key='movie.id'>
+    <span>{{movie.original_title}}</span>
+    </li>
+</ul>
+</template>
