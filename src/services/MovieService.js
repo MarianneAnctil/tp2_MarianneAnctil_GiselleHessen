865d85b7c4536df2dc7fbf4f8fe8b031881c40df
Marianne Anctil <2163768@csfoy.ca>
2023-12-20 21:40:48 -0500

    changement page

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -8,31 +8,30 @@ let headers = {
 };
 
 export async function getAllMovies() {
-    const response = await fetch(baseURLDiscover+ '?api_key=daae932b052d96ade399f33a4e6a9e58&year=2023', headers);
+    const response = await fetch(baseURLDiscover + '?api_key=daae932b052d96ade399f33a4e6a9e58&year=2023', headers);
     return await response.json();
 }
 
 export async function getMovie(id) {
-    const response = await fetch('https://api.themoviedb.org/3/movie/'+ id +'?api_key=daae932b052d96ade399f33a4e6a9e58', headers);
+    const response = await fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=daae932b052d96ade399f33a4e6a9e58', headers);
     let movie = await response.json();
     return movie;
 }
 
-export async function getMovieKeyword(keyWord) {
-    const response = await fetch(baseURLSearch+ '?api_key=daae932b052d96ade399f33a4e6a9e58&query='+keyWord, headers);
+export async function getMovieKeyword(keyWord, page) {
+    const response = await fetch(baseURLSearch + '?api_key=daae932b052d96ade399f33a4e6a9e58&query=' + keyWord + '&page=' + page, headers);
     let movie = await response.json();
     return movie;
 }
-export async function getMovieDate(date) {
-    const response = await fetch(baseURLDiscover+ '?api_key=daae932b052d96ade399f33a4e6a9e58&year='+date, headers);
+export async function getMovieDate(date, page) {
+    const response = await fetch(baseURLDiscover + '?api_key=daae932b052d96ade399f33a4e6a9e58&year=' + date + '&page=' + page, headers);
     let movie = await response.json();
     return movie;
 }
 
 export async function getMovieGenre(genre) {
-    const response = await fetch (baseURLDiscover+ '?api_key=daae932b052d96ade399f33a4e6a9e58&with_genre='+genre, headers);
+    const response = await fetch(baseURLDiscover + '?api_key=daae932b052d96ade399f33a4e6a9e58&with_genre=' + genre, headers);
     let movie = await response.json();
-    console.log(response)
     return movie;
 }
 
